f1fc67421a82f199515f1ded6e77dca3bd44927d
Pradyumna Anil Kumar Kubear <57552746+PradyumnaA@users.noreply.github.com>
2023-11-19 22:28:36 +0000

    Completed Settings And Added ActiveMenu

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,7 +1,52 @@
 import React from 'react'
-
-export const App = () => {
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import { FiSettings } from 'react-icons/fi';
+import {TooltipComponent} from '@syncfusion/ej2-react-popups'
+import './App.css'
+const App = () => {
+  const activeMenu=true
   return (
-    <h1 className='underline text-3xl'>App</h1>
+   <div>
+    <BrowserRouter>
+    <div className='flex relative dark:bg-main-dark-bg'>
+      <div className='fixed right-4 bottom-4' style={{
+        zIndex:'1000'
+      }}>
+        <TooltipComponent content="Settings"
+         position='Top'>
+          <button
+          type="button"
+          className='text-3xl p-3
+          hover:drop-shadow-xl
+          hover:bg-light-gray
+          text-white'
+          style={{
+            background:'#5C5470',
+            borderRadius:'70%'
+          }}
+          
+          >
+            <FiSettings/>
+          </button>
+        </TooltipComponent>
+      </div>
+      {
+        activeMenu ?
+        (
+          <div>
+              Sidebar
+          </div>
+        )
+        :
+        (
+          <div>
+            Sidebar w-0
+            </div>
+        )
+      }
+    </div>
+    </BrowserRouter>
+   </div>
   )
 }
+export default App
--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,4 +1,9 @@
 import React from 'react';
-import { ReactDOM } from 'react-dom';
-import App from './App'
-ReactDOM.render(<App/>,document.getElementById('root'))
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import App from './App';
+import './index.css'
+
+ReactDOM.render(<App />, document.getElementById('root'));
