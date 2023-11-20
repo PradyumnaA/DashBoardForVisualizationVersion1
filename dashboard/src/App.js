9c924bee15ae9d1d2791dcd854b5a621029ea2e0
Pradyumna Anil Kumar Kubear <57552746+PradyumnaA@users.noreply.github.com>
2023-11-20 02:38:34 +0000

    Navbar commit

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -4,7 +4,7 @@ import { FiSettings } from 'react-icons/fi';
 import {TooltipComponent} from '@syncfusion/ej2-react-popups'
 import './App.css'
 const App = () => {
-  const activeMenu=true
+  const activeMenu=false
   return (
    <div>
     <BrowserRouter>
@@ -33,17 +33,32 @@ const App = () => {
       {
         activeMenu ?
         (
-          <div>
+          <div className='
+          w-72 fixed sidebar dark:bg-secondary-dark-bg 
+          bg-white
+          '>
               Sidebar
           </div>
         )
         :
         (
-          <div>
+          <div className='w-0 dark:bg-secondary-dark-bg'>
             Sidebar w-0
             </div>
         )
       }
+    <div className={
+      `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu?
+      'md:ml-72':'flex-2'}`
+    }>
+      <div className='fixed md:static bg-main-bg
+      dark:bg-main-dark-bg
+      navbar w-full'>
+        Navbar
+
+      </div>
+      </div>
+      
     </div>
     </BrowserRouter>
    </div>
